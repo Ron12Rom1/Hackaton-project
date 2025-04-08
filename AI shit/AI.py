45d826fa1ae8c6e49758277dd4ee258f11455b31
rezero567 <itayrabayev2000@gmail.com>
2025-04-08 08:08:38 +0300

    fuck you itay!!!

---
+++ AI shit/AI.py
@@ -0,0 +1,48 @@
+from groq import Groq
+import dotenv
+
+import time
+
+
+client = Groq(
+    api_key=dotenv.get_key('.env', 'SECRET_KEY'),
+)
+
+with open("AI shit/you_are.txt", "r") as f1:
+    you_are = f1.read()
+
+def send_to_AI(message):
+        
+    with open ("AI shit/memory.peepee_poopoo", "r+") as mem:
+        memory = mem.read()
+
+        chat_completion = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": str(you_are) + "It is: " + str(time.time()) + 
+             ".   this is what you remember from our previous conversation: " + str(memory)},
+
+            {"role": "user", "content": message}
+        ],
+        model="llama3-70b-8192",
+        stream=False,
+        top_p=1,
+        temperature=1.5,
+        frequency_penalty=2.0,
+        presence_penalty=2.0,
+    )
+
+    output = chat_completion.choices[0].message.content
+
+    print("\n", output)
+    # pyttsx3_TTS(output)
+
+    with open("AI shit/memory.peepee_poopoo", "a") as mem:
+        mem.write("\nUser: " + message  + "\nYou: " + output)
+
+    return output
+
+if __name__ == "__main__":
+    userIn = "-1"
+    while userIn != "exit":
+        userIn = input(":   ")
+        print(send_to_AI(userIn))
